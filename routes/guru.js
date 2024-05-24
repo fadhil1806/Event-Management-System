e00ada5fa05af075fdcb0cfb7f278d2a9f6e59c6
fadhil1806 <fadhil8637@smk.belajar.id>
2024-05-24 15:45:09 +0700

    result project-event

---
+++ routes/guru.js
@@ -0,0 +1,22 @@
+var express = require('express');
+const { registerGuru, getDataGuru, loginGuru } = require('../controllers/guru');
+const validate = require('../middleware/validate');
+const { body } = require('express-validator');
+const router = express.Router();
+
+const validation = [
+    body("name").notEmpty().withMessage("Name is required"),
+    body("username").notEmpty().withMessage("Username is required"), 
+    body("password").notEmpty().withMessage("Password is required")
+];
+
+const validationLogin = [
+  body("username").notEmpty().withMessage("Username is required"), 
+  body("password").notEmpty().withMessage("Password is required")
+]
+
+router.get('/', getDataGuru);
+router.post('/create', validate(validation), registerGuru);
+router.post('/login', validate(validationLogin), loginGuru)
+
+module.exports = router;
